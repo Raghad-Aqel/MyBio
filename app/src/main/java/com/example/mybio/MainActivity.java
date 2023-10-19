1d3e778aa6c9cd68c98bde9382856bca5b2a7fc3
97059 <raghad.saleem.aqel@gmail.com>
2023-10-19 17:05:38 +0300

    vertion1

---
+++ app/src/main/java/com/example/mybio/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mybio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSave;
+    private TextView txtHobbies;
+    private EditText edtHobbies;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSave = findViewById(R.id.btnSave);
+        txtHobbies = findViewById(R.id.txtHobbies);
+        edtHobbies = findViewById(R.id.adtHobbies);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String hobbies = edtHobbies.getText().toString();
+                if (! hobbies.isEmpty()){
+                    txtHobbies.setText(hobbies);
+                    txtHobbies.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+    }
+}
